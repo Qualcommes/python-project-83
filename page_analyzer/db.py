115c4b7b1004fb7b4e21c325086b7ff6f4798868
Qualcommes <qualcommes122@gmail.com>
2026-09-09 11:26:55 +0700

    fix testing connection

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 
 def get_db_connection():
-    database_url = os.getenv('DATABASE_URL')
+    database_url = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@localhost:5432/page_analyzer')
     return psycopg.connect(database_url, row_factory=dict_row)
 
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,12 @@
+import os
 import pytest
 from page_analyzer import app
 
+
+os.environ['DATABASE_URL'] = os.getenv(
+    'DATABASE_URL',
+    'postgresql://postgres:postgres@localhost:5432/page_analyzer'
+)
 
 @pytest.fixture
 def client():
